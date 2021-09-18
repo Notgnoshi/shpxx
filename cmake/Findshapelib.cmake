37c6e82c49a4590553b40c28ce5f5dbdac658650
Andrew Stelter <andrew.d.stelter@gmail.com>
2021-09-17 21:31:05 -0500

    Add shapelib dependency
    This will find and add shapelib similarly to how we are finding and
    adding googletest with FetchContent.
    
    By default, shapelib will attempt to fetch test data so that it can be
    used for tests. This did not work out of the box, and we were unable
    to determine how to make it work; so we opted to disable the testing
    from shapelib altogether.
    
    The shapelib cmake files do not provide a method to disable testing; it
    looks like you should be able to use -DBUILD_TEST=OFF to disable them,
    but because the variable is not a CACHE or option() variable, this
    doesn't actually work. In order to disable the tests, we are applying
    a patch step after fetching the library which will use a cmake
    sub-invocation to replace set(BUILD_TEST ON) with set(BUILD_TEST OFF)
    throughout the project's cmake file.

---
+++ cmake/Findshapelib.cmake
@@ -0,0 +1,26 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    shapelib
+    GIT_REPOSITORY https://github.com/OSGeo/shapelib.git
+    GIT_TAG "master"
+    PATCH_COMMAND
+        ${CMAKE_COMMAND} -DFILE=${CMAKE_CURRENT_BINARY_DIR}/_deps/shapelib-src/CMakeLists.txt -P
+        ${CMAKE_CURRENT_LIST_DIR}/DisableBuildTest.cmake
+)
+
+FetchContent_GetProperties(shapelib)
+if(NOT shapelib_POPULATED)
+    FetchContent_Populate(shapelib)
+
+    # When building shapelib along with this project, we want to always build shapelib as a shared
+    # library, regardless of the BUILD_SHARED_LIBS variable
+    set(OLD_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
+    set(BUILD_SHARED_LIBS ON)
+
+    add_subdirectory(${shapelib_SOURCE_DIR} ${shapelib_BINARY_DIR} EXCLUDE_FROM_ALL)
+
+    set(BUILD_SHARED_LIBS ${OLD_BUILD_SHARED_LIBS})
+endif()
+
+add_library(shapelib::shp ALIAS shp)
