d3eadc5ca0209d7f13c62e7b7026ee9dc970f32c
Andrew Stelter <andrew.d.stelter@gmail.com>
2021-09-18 16:04:52 -0500

    Configure version constants during build

--- include/shpxx/version.hpp
+++ include/shpxx/version.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
 #include <cstddef>
+#include <string>
 
 namespace shpxx {
-std::size_t version();
-}
+struct version_t
+{
+    const static std::size_t major;
+    const static std::size_t minor;
+    const static std::size_t patch;
+
+    const static std::string version_str;
+    const static std::string git_hash;
+    const static std::string build_timestamp;
+};
+}  // namespace shpxx
--- tests/test_version.cpp
+++ tests/test_version.cpp
@@ -3,5 +3,11 @@
 #include <gtest/gtest.h>
 TEST(Shpxx, Version)
 {
-    ASSERT_EQ(shpxx::version(), 0u);
+    ASSERT_GE(shpxx::version_t::major, 0);
+    ASSERT_GE(shpxx::version_t::minor, 0);
+    ASSERT_GE(shpxx::version_t::patch, 0);
+
+    ASSERT_GT(shpxx::version_t::version_str.size(), 0);
+    ASSERT_GT(shpxx::version_t::build_timestamp.size(), 0);
+    ASSERT_GT(shpxx::version_t::git_hash.size(), 0);
 }
