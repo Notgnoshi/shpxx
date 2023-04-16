c77e13d113e0312c69baa1cb76e4fa0a07b02fd7
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-16 09:26:55 -0500

    Return shpfile_t from open_shp

--- src/io/file.cpp
+++ src/io/file.cpp
@@ -2,13 +2,17 @@
 
 #include "io/file_handler.hpp"
 #include "io/safile_wrapper.hpp"
+#include "shp_casts.hpp"
+#include "shpxx/exceptions.hpp"
 
 #include <shapefil.h>
 
 namespace shpxx::io::file {
-void open_shp(const std::string& file_path, shpxx::io::openmode mode)
+shpxx::shpfile_t open_shp(const std::string& file_path, shpxx::io::openmode mode)
 {
     auto sa_hooks = make_sahooks<file_handler_t>();
-    SHPOpenLL(file_path.c_str(), to_string(mode).c_str(), &sa_hooks);
+    auto* file_handle = SHPOpenLL(file_path.c_str(), to_string(mode).c_str(), &sa_hooks);
+
+    return shpxx::shpfile_t(shp_handle_t(cast_shp_info(file_handle)));
 }
 }  // namespace shpxx::io::file
