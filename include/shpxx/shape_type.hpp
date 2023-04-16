d1317747045e765d359c05342ec2e42e01bb7a34
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-16 09:10:38 -0500

    Add shape-type enum

---
+++ include/shpxx/shape_type.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace shpxx {
+//! @brief Enumeration of types of shapes that can be in a file
+//!
+//! The definitions of these enum values is defined in the ESRI shapefile
+//! standard and should not be changed.
+enum class shape_type
+{
+    null = 0,
+    point = 1,
+    arc = 3,
+    polygon = 5,
+    multi_point = 8,
+    point_z = 11,
+    arc_z = 13,
+    polygon_z = 15,
+    multi_point_z = 18,
+    point_m = 21,
+    arc_m = 23,
+    polygon_m = 25,
+    multi_point_m = 28,
+    multi_patch = 31
+};
+}  // namespace shpxx
