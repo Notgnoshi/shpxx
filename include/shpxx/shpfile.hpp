477a68a83093d54fe37c6636854e902870692a96
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-16 09:26:46 -0500

    Define "file-level" API type

---
+++ include/shpxx/shpfile.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "shpxx/geometry/point.hpp"
+#include "shpxx/shape_type.hpp"
+#include "shpxx/shp_handle.hpp"
+
+#include <cstdint>
+
+namespace shpxx {
+
+//! @brief Handle representing an opened shp file
+//!
+//! The shpfile_t can be used to query file-level details and access the
+//! individual elements of the file.
+class shpfile_t
+{
+  public:
+    //! @brief Construct a shpfile_t for a specific file handle
+    //!
+    //! The shpfile_t will take ownership of the file handle and close it when
+    //! it is destroyed.
+    //!
+    //! @param[in] handle Pointer to the handle for the opened file. Must not be
+    //! null.
+    explicit shpfile_t(shp_handle_t handle);
+    ~shpfile_t() = default;
+
+    shpfile_t(const shpfile_t&) = delete;
+    shpfile_t& operator=(const shpfile_t&) = delete;
+
+    shpfile_t(shpfile_t&&) = default;
+    shpfile_t& operator=(shpfile_t&&) = default;
+
+    //! @brief Get the number of elements in the file, including NULL elements
+    [[nodiscard]] std::size_t size() const noexcept;
+
+    //! @brief Get the type of shapes in the file
+    //!
+    //! @note According to the SHP standard, any given file is only allowed to
+    //! contain one type of shape. However, it's not uncommon for user-data to
+    //! contain multiple types, and there's technically nothing that prevents
+    //! creating a non-standard file that does so. This returns the type that
+    //! the file header indicates is stored, but there is no guarantee that it
+    //! actually holds true for all shapes in the file.
+    [[nodiscard]] shape_type type() const noexcept;
+
+    //! @brief Minimum x, y, z, m values of the file
+    //!
+    //! @note These are the values stored in the file header, and as such may
+    //! not accurately reflect the data in the file.
+    //!
+    //! @return A point_m_t containing the minimum bound values. The values of
+    //! z and m should be 0 when they are unused, but this is not guaranteed.
+    [[nodiscard]] geometry::point_m_t min_bound() const noexcept;
+
+    //! @brief Maximum x, y, z, m values of the file
+    //!
+    //! @note These are the values stored in the file header, and as such may
+    //! not accurately reflect the data in the file.
+    //!
+    //! @return A point_m_t containing the minimum bound values. The values of
+    //! z and m should be 0 when they are unused, but this is not guaranteed.
+    [[nodiscard]] geometry::point_m_t max_bound() const noexcept;
+
+  private:
+    shp_handle_t m_handle;
+
+    std::size_t m_num_shapes = 0;
+    shape_type m_shape_type = shape_type::null;
+    geometry::point_m_t m_min_bound = {};
+    geometry::point_m_t m_max_bound = {};
+};
+}  // namespace shpxx
---
+++ src/shpfile.cpp
@@ -0,0 +1,50 @@
+#include "shpxx/shpfile.hpp"
+
+#include "shp_casts.hpp"
+
+#include <shapefil.h>
+
+#include <array>
+#include <cassert>
+
+namespace shpxx {
+//! @brief Convert an array of doubles into a point
+static geometry::point_m_t make_point(const std::array<double, 4>& data);
+
+shpfile_t::shpfile_t(shp_handle_t handle) : m_handle(std::move(handle))
+{
+    assert(m_handle);
+
+    int number_of_shapes = 0;
+    int shape_type = SHPT_NULL;
+    std::array<double, 4> min_bound{};
+    std::array<double, 4> max_bound{};
+
+    SHPGetInfo(cast_shp_info(m_handle.get()),
+               &number_of_shapes,
+               &shape_type,
+               min_bound.data(),
+               max_bound.data());
+
+    m_shape_type = static_cast<shpxx::shape_type>(shape_type);
+    m_num_shapes = static_cast<std::size_t>(number_of_shapes);
+    m_min_bound = make_point(min_bound);
+    m_max_bound = make_point(max_bound);
+}
+
+std::size_t shpfile_t::size() const noexcept { return m_num_shapes; }
+shape_type shpfile_t::type() const noexcept { return m_shape_type; }
+geometry::point_m_t shpfile_t::min_bound() const noexcept { return m_min_bound; }
+geometry::point_m_t shpfile_t::max_bound() const noexcept { return m_max_bound; }
+
+geometry::point_m_t make_point(const std::array<double, 4>& data)
+{
+    geometry::point_m_t point{};
+    point.x = data[0];
+    point.y = data[1];
+    point.z = data[2];
+    point.m = data[3];
+    return point;
+}
+
+}  // namespace shpxx
