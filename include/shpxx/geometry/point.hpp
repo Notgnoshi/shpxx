1db8e71939b7b6a0f64b0782c0b0a020e1fa259c
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-16 09:16:57 -0500

    Define geometric point types

---
+++ include/shpxx/geometry/point.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace shpxx::geometry {
+struct point_t
+{
+    double x;
+    double y;
+};
+
+struct point_z_t
+{
+    double x;
+    double y;
+    double z;
+};
+
+struct point_m_t
+{
+    double x;
+    double y;
+    double z;
+    double m;
+};
+}  // namespace shpxx::geometry
