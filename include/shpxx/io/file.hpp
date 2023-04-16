586aeb13a0b114e5ae369e076ed4f6ff728cd266
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-16 09:26:55 -0500

    Return shpfile_t from open_shp

--- include/shpxx/io/file.hpp
+++ include/shpxx/io/file.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
-#include "openmode.hpp"
+#include "shpxx/io/openmode.hpp"
+#include "shpxx/shpfile.hpp"
 
 #include <string>
 
 namespace shpxx::io::file {
-void open_shp(const std::string& file_path, shpxx::io::openmode mode);
-}
+//! @brief Open a shape file
+//!
+//! This will open not only the root .shp file, but other associated files that
+//! can be found next to it.
+//!
+//! @param[in] file_path Path to the .shp file to open
+//! @param[in] mode Mode to open the file in
+//!
+//! @return A shpfile_t handle to the file
+shpxx::shpfile_t open_shp(const std::string& file_path, shpxx::io::openmode mode);
+}  // namespace shpxx::io::file
