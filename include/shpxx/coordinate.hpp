b6feec68343bd574a777b125e6a6f17025c4f4f6
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-30 11:45:39 -0500

    Rename geometry::point_* to coordinate_
    This differentiates it from the point_t feature type, and removes the
    geometry:: namespace entirely. Eventually, we might want to move the
    coordinate types into feature::; but for now, they'll live with the other
    shpxx primitives

--- include/shpxx/coordinate.hpp
+++ include/shpxx/coordinate.hpp
@@ -1,31 +1,31 @@
 #pragma once
 
-namespace shpxx::geometry {
-struct point_xy_t
+namespace shpxx {
+struct coordinate_xy_t
 {
     double x;
     double y;
 };
 
-struct point_xym_t
+struct coordinate_xym_t
 {
     double x;
     double y;
     double m;
 };
 
-struct point_xyz_t
+struct coordinate_xyz_t
 {
     double x;
     double y;
     double z;
 };
 
-struct point_xyzm_t
+struct coordinate_xyzm_t
 {
     double x;
     double y;
     double z;
     double m;
 };
-}  // namespace shpxx::geometry
+}  // namespace shpxx
