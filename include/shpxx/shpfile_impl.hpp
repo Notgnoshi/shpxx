51aceee2fcaeb403819853f92a4bc44ba44e4238
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-30 09:45:47 -0500

    Define shpfile_t iterators

--- include/shpxx/shpfile_impl.hpp
+++ include/shpxx/shpfile_impl.hpp
@@ -13,6 +13,27 @@
 namespace shpxx {
 
 template<shpxx::concepts::IsFeature FeatureT>
+auto shpfile_t::begin() const -> const_feature_iterator_t<FeatureT>
+{
+    throw_if_incompatible<FeatureT>();
+
+    if (size() > 0)
+    {
+        return const_feature_iterator_t<FeatureT>(*this, 0);
+    }
+
+    return end<FeatureT>();
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
+auto shpfile_t::end() const -> const_feature_iterator_t<FeatureT>
+{
+    throw_if_incompatible<FeatureT>();
+
+    return const_feature_iterator_t<FeatureT>(*this);
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
 auto shpfile_t::at(std::size_t index) const -> std::optional<FeatureT>
 {
     if (index >= size())
@@ -41,5 +62,73 @@ void shpfile_t::throw_if_incompatible() const
     }
 }
 
+template<shpxx::concepts::IsFeature FeatureT>
+shpfile_t::const_feature_iterator_t<FeatureT>::const_feature_iterator_t(const shpfile_t& file) :
+    m_file(file), m_curr_index(std::numeric_limits<std::size_t>::max()), m_curr_obj(std::nullopt)
+{
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
+shpfile_t::const_feature_iterator_t<FeatureT>::const_feature_iterator_t(const shpfile_t& file,
+                                                                        std::size_t index) :
+    m_file(file), m_curr_index(index), m_curr_obj(m_file.at<FeatureT>(m_curr_index))
+{
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
+auto shpfile_t::const_feature_iterator_t<FeatureT>::operator*() const noexcept -> reference
+{
+    return m_curr_obj;
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
+auto shpfile_t::const_feature_iterator_t<FeatureT>::operator->() const noexcept -> pointer
+{
+    return &m_curr_obj;
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
+auto shpfile_t::const_feature_iterator_t<FeatureT>::operator++() noexcept
+    -> const_feature_iterator_t&
+{
+    if (m_curr_index != std::numeric_limits<std::size_t>::max())
+    {
+        if (++m_curr_index < m_file.size())
+        {
+            m_curr_obj = m_file.at<FeatureT>(m_curr_index);
+        } else
+        {
+            m_curr_index = std::numeric_limits<std::size_t>::max();
+        }
+    }
+    return *this;
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
+auto shpfile_t::const_feature_iterator_t<FeatureT>::operator++(int) noexcept
+    -> const_feature_iterator_t
+{
+    auto self_move = const_feature_iterator_t<FeatureT>(m_file);
+    self_move.m_curr_index = m_curr_index;
+    self_move.m_curr_obj = std::move(m_curr_obj);
+
+    this->operator++();
+
+    return self_move;
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
+auto shpfile_t::const_feature_iterator_t<FeatureT>::operator==(
+    shpfile_t::const_feature_iterator_t<FeatureT> other) const noexcept -> bool
+{
+    return &m_file == &other.m_file && m_curr_index == other.m_curr_index;
+}
+
+template<shpxx::concepts::IsFeature FeatureT>
+auto shpfile_t::const_feature_iterator_t<FeatureT>::operator!=(
+    shpfile_t::const_feature_iterator_t<FeatureT> other) const noexcept -> bool
+{
+    return !(*this == other);
+}
 
 }  // namespace shpxx
