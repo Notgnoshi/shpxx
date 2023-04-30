9e84b6259a6feb1a3afa608151a5b7d1167e4124
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-30 10:03:47 -0500

    Add opaque pointer wrapper for SHPObject

--- src/shp_handle.cpp
+++ src/shp_handle.cpp
@@ -7,4 +7,6 @@
 namespace shpxx::shplib {
 void close_file_t::operator()(shplib::shp_info* p) const { SHPClose(cast_shp_info(p)); }
 
+void destroy_object_t::operator()(shp_object* p) const { SHPDestroyObject(cast_shp_object(p)); }
+
 }  // namespace shpxx::shplib
