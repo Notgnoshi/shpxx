8d83b9fabe2d7cf9bf66d283afe1ba383fd27270
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-30 12:31:03 -0500

    Add check for "no data" values

--- include/shpxx/coordinate.hpp
+++ include/shpxx/coordinate.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 namespace shpxx {
 struct coordinate_xy_t
 {
@@ -28,4 +30,11 @@ struct coordinate_xyzm_t
     double z;
     double m;
 };
+
+//! @brief Per the ESRI shapefile specification, any Z, M, or otherwise "nullable" field
+//! containing a value smaller than this is considered to contain no data.
+constexpr static double no_data_threshold = 1E-38;
+
+//! @brief Check if a value is "no data" by the definition in the ESRI standard
+constexpr bool is_no_data(double value) noexcept { return std::abs(value) < no_data_threshold; }
 }  // namespace shpxx
