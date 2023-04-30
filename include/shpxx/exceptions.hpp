212001144ced849fc66f6fefec212408973ccfca
Andrew Stelter <andrew.d.stelter@gmail.com>
2023-04-30 09:31:46 -0500

    Add incompatible feature type exception

--- include/shpxx/exceptions.hpp
+++ include/shpxx/exceptions.hpp
@@ -18,4 +18,12 @@ class invalid_file_error_t : public error_t
     using error_t::error_t;
 };
 
+//! Indicates that an attempt was made to access features in the file as an
+//! incorrect type
+class incompatible_feature_type_error_t : public error_t
+{
+  public:
+    using error_t::error_t;
+};
+
 }  // namespace shpxx
