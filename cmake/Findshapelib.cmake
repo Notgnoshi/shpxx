9dbf3d2a3e6115df92a4dd49c761a85e6b7d84f2
Austin Gill <Notgnoshi@gmail.com>
2026-05-14 13:57:44 -0500

    Update Findshapelib for shapelib 1.6.3

--- cmake/Findshapelib.cmake
+++ cmake/Findshapelib.cmake
@@ -3,10 +3,8 @@ include(FetchContent)
 FetchContent_Declare(
     shapelib
     GIT_REPOSITORY https://github.com/OSGeo/shapelib.git
-    GIT_TAG "master"
+    GIT_TAG "v1.6.3"
     SOURCE_DIR ${PROJECT_BINARY_DIR}/_deps/shapelib-src
-    PATCH_COMMAND ${CMAKE_COMMAND} -DFILE=${PROJECT_BINARY_DIR}/_deps/shapelib-src/CMakeLists.txt
-                  -P ${CMAKE_CURRENT_LIST_DIR}/DisableBuildTest.cmake
 )
 
 FetchContent_GetProperties(shapelib)
@@ -16,12 +14,15 @@ if(NOT shapelib_POPULATED)
     # When building shapelib along with this project, we want to always build shapelib as a shared
     # library, regardless of the BUILD_SHARED_LIBS variable
     set(OLD_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
+    set(OLD_BUILD_TESTING ${BUILD_TESTING})
     set(BUILD_SHARED_LIBS ON)
+    set(BUILD_TESTING OFF)
 
     add_subdirectory(${shapelib_SOURCE_DIR} ${shapelib_BINARY_DIR} EXCLUDE_FROM_ALL)
     target_include_directories(shp INTERFACE $<BUILD_INTERFACE:${shapelib_SOURCE_DIR}>)
 
     set(BUILD_SHARED_LIBS ${OLD_BUILD_SHARED_LIBS})
+    set(BUILD_TESTING ${OLD_BUILD_TESTING})
 endif()
 
 add_library(shapelib::shp ALIAS shp)
